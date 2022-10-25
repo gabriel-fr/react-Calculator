4438302d7f33bca34029bc4cb76fd6f48c27c5ad
Gabriel Ribeiro <gabriel@enzo.com.br>
2022-10-25 14:45:33 -0300

    ultimo ajuste

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,23 +1,71 @@
 import { StyledCalculator } from "./styles";
 import Button from "../Button/Button";
 import Display from "../Display/Display";
+import { useState } from "react";
 
 const Calculator = () => {
+  const initialState = {
+    displayValue: "0",
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0,
+  };
+
+  const [params, setParams] = useState(initialState);
+
   const clearMemory = () => {
-    console.log("limpar");
+    setParams(initialState);
   };
 
   const setOperation = (op) => {
-    console.log(op);
+    if (params.current === 0) {
+      setParams({ ...params, operation: op, current: 1, clearDisplay: true });
+      return;
+    }
+
+    const equals = op === "=";
+    const currentOperation = params.operation;
+    const values = params.values;
+
+    try {
+      values[0] = eval(`${values[0]} ${currentOperation} ${values[1]} `);
+    } catch (e) {
+      values[0] = params.values[0];
+    }
+    values[1] = 0;
+
+    setParams({
+      ...params,
+      displayValue: values[0],
+      operation: equals ? null : op,
+      current: equals ? 0 : 1,
+      clearDisplay: !equals,
+      values,
+    });
   };
 
   const addDigit = (digit) => {
-    console.log(digit);
+    if (digit === "." && params.displayValue.includes(".")) return;
+
+    const clearDisplay = params.displayValue === "0" || params.clearDisplay;
+    const currentValue = clearDisplay ? "" : params.displayValue;
+    const displayValue = currentValue + digit;
+
+    setParams({ ...params, displayValue, clearDisplay: false });
+
+    if (digit !== ".") {
+      const currentPosition = params.current;
+      const newValue = displayValue;
+      const parseValue = parseFloat(newValue);
+      const newValues = params.values;
+      newValues[currentPosition] = parseValue;
+    }
   };
 
   return (
     <StyledCalculator>
-      <Display value="100" />
+      <Display value={params.displayValue} />
       <Button variant="standard" type="huge" onClick={(e) => clearMemory()} label="AC" />
       <Button variant="action" onClick={(e) => setOperation(e.target.textContent)} label="/" />
       <Button variant="standard" onClick={(e) => addDigit(e.target.textContent)} label="7" />
